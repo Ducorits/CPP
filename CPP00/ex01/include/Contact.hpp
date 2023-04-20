26453bbd91ca2b036b47355a207109cebdd6402b
Duco Ritsema van Eck <ducorits@zonderstreepje.nl>
2023-04-20 18:57:46 +0200

    Added nothing really

--- CPP00/ex01/include/Contact.hpp
+++ CPP00/ex01/include/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: dritsema <dritsema@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/18 13:43:04 by dritsema      #+#    #+#                 */
-/*   Updated: 2023/04/19 18:33:10 by dritsema      ########   odam.nl         */
+/*   Updated: 2023/04/20 14:05:51 by dritsema      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,17 @@
 class Contact
 {
 	public:
-		void	print(void);
-		void
+		void		print(void);
+		void		setFirstName(std::string str);
+		std::string	getFirstName(void);
+		void		setLasttName(std::string str);
+		std::string	getLasttName(void);
+		void		setNickname(std::string str);
+		std::string	getNickname(void);
+		void		setPhoneNumber(std::string str);
+		std::string	getPhoneNumber(void);
+		void		setDarkestSecret(std::string str);
+		std::string	getDarkestSecret(void);
 
 	private:
 		std::string	first_name;
