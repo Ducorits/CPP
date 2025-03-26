cba430420058f0f185d9c35086313695402659df
Duco Ritsema van eck <dritsema@f1r3s9.codam.nl>
2025-03-26 19:08:18 +0100

    cpp 09 stuff

--- CPP09/ex01/main.cpp
+++ CPP09/ex01/main.cpp
@@ -3,6 +3,36 @@
 #include <regex>
 #include <iostream>
 
+void solve_rpn(std::string argument)
+{
+	std::stack<double> stack;
+	double a;
+	double b;
+
+	for (auto it = argument.begin(); it != argument.end(); it++)
+	{
+		if (isdigit(*it))
+		{
+			stack.push(std::stod(&*it));
+		}
+		else
+		{
+			a = stack.top();
+			stack.pop();
+			b = stack.top();
+			stack.pop();
+			if (*it == '+')
+				stack.push(a + b);
+			else if (*it == '-')
+				stack.push(a - b);
+			else if (*it == '/')
+				stack.push(a / b);
+			else if (*it == '*')
+				stack.push(a * b);
+		}
+	}
+}
+
 int main(int argc, char **argv)
 {
 	std::stack<std::string> stack;
@@ -14,4 +44,16 @@ int main(int argc, char **argv)
 		std::cerr << "Invalid argument count! should be at least 1" << std::endl;
 		return (1);
 	}
+	std::smatch match;
+	std::string argument(argv[1]);
+	if (std::regex_match(argument, match, pattern))
+	{
+		solve_rpn(argument);
+	}
+	else
+	{
+		std::cerr << "Invalid argument: " << argument << std::endl;
+	}
+
+
 }
