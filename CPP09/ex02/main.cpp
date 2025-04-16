61ab5fcb0899feeb03a718cedb90cb703cd4895c
Duco Ritsema van eck <dritsema@f1r4s12.codam.nl>
2025-04-16 19:17:41 +0200

    It's a good start. Let's finish tommorrow

--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -3,17 +3,12 @@
 #include <vector>
 #include <deque>
 
-// void deqBinaryInsert(std::deque<int> &sorted_deq, int value)
-// {
-// }
-
-// void deqFordJohnsonSort(std::deque<int> &deq)
-// {
-// }
-
-// void vecBinaryInsert(std::vector<int> &sorted_vec, int value)
-// {
-// }
+typedef struct number_s {
+	int value;
+	int index;
+	number_s *winner;
+	number_s *loser;
+} number_t;
 
 void print_vec(std::vector<int> &vec)
 {
@@ -24,17 +19,94 @@ void print_vec(std::vector<int> &vec)
 	std::cout << std::endl;
 }
 
+void print_number(number_t num)
+{
+	std::cout << num.value << " ";
+}
+
+void print_numvec(std::vector<number_t *> &vec)
+{
+	for (auto value : vec)
+	{
+		print_number(*value);
+	}
+	std::cout << std::endl;
+}
+
+void insert(number_t *num, size_t start_index, size_t end_index, std::vector<number_t *> &vec)
+{
+	size_t sectionSize = end_index - start_index + 1;
+	size_t compare_index = start_index + end_index / 2;
+	if (sectionSize == 1)
+	{
+		auto it = vec.begin() + compare_index;
+		vec.insert(it, num);
+	}
+	else if (num->value <= vec[compare_index]->value)
+	{
+		insert(num, start_index, compare_index - 1, vec);
+	}
+	else if (num->value > vec[compare_index]->value)
+	{
+		insert(num, compare_index + 1, end_index, vec);
+	}
+}
+
+void fordJohnsonSort(std::vector<number_t *> &vec)
+{
+	std::vector<number_t *> winners;
+	size_t i = 0;
+
+	print_numvec(vec);
+
+	for (; i + 1 < vec.size(); i++)
+	{
+		std::cout << "comparing: " << vec[i]->value << " and " << vec[i + 1]->value << std::endl;
+		if (vec[i]->value >= vec[i + 1]->value)
+		{
+			auto it = vec.begin() + i;
+			winners.push_back(vec[i]);
+			(*it)->index = winners.size() - 1;
+			(*it)->loser = vec[i + 1];
+			vec[i + 1]->winner = *it;
+			vec.erase(it);
+		}
+		else
+		{
+			auto it = vec.begin() + i + 1;
+			winners.push_back(vec[i + 1]);
+			(*it)->index = winners.size() - 1;
+			(*it)->loser = vec[i];
+			vec[i]->winner = *it;
+			vec.erase(it);
+		}
+	}
+
+	std::cout << "winners: " << std::endl;
+	print_numvec(winners);
+	std::cout << "Rest: " << std::endl;
+	print_numvec(vec);
+
+	if (winners.size() > 1)
+		fordJohnsonSort(winners);
+
+	// Start insertion. Use jacobsthal sequence. (make binary insert to start)
+	for (auto value : vec)
+	{
+		insert(value, 0, winners.size() - 1, winners);
+	}
+	vec = winners;
+}
+
 void vecFordJohnsonSort(std::vector<int> &vec, int offset)
 {
-	size_t i = offset;
-	size_t index_offset;
-	size_t i2;
-	size_t sectionSize = vec.size() - offset;
+	int i = offset;
+	int index_offset;
+	int i2;
+	int sectionSize = vec.size() - offset;
 
 	if (sectionSize < 2)
 		return;
-
-	print_vec(vec);
 
 	if (sectionSize % 2 == 0)
 		i2 = offset + (sectionSize / 2);
@@ -42,8 +114,14 @@ void vecFordJohnsonSort(std::vector<int> &vec, int offset)
 		i2 = offset + (sectionSize / 2 + 1);
 
 	index_offset = i2;
+	print_vec(vec);
+	std::cout << "offset: " << offset << std::endl;
+	std::cout << "index_offset: " << index_offset << std::endl;
+	std::cout << "SectionSize: " << sectionSize << std::endl;
+	std::cout << "i: " << i << std::endl;
+	std::cout << "i2: " << i2 << std::endl;
 
-	while (i < sectionSize / 2)
+	while (i - offset < sectionSize / 2)
 	{
 		int tmp;
 
@@ -56,14 +134,17 @@ void vecFordJohnsonSort(std::vector<int> &vec, int offset)
 		i++;
 		i2++;
 	}
+
+	print_vec(vec);
 	if (sectionSize > 1)
-		vecFordJohnsonSort(vec, offset + index_offset);
+		vecFordJohnsonSort(vec, index_offset);
 }
 
 int PmergeMe(char **argv)
 {
 	std::vector<int> vec;
 	std::deque<int> deq;
+	std::vector<number_t *> num_vec;
 
 	std::regex pattern(R"(^[0-9]+$)");
 
@@ -77,6 +158,13 @@ int PmergeMe(char **argv)
 			int num = std::stoi(argument);
 			vec.push_back(num);
 			deq.push_back(num);
+
+			number_t *nur = new number_t;
+			nur->index = num_vec.size() - 1;
+			nur->value = num;
+			nur->loser = 0;
+			nur->winner = 0;
+			num_vec.push_back(nur);
 		}
 		else
 		{
@@ -85,12 +173,17 @@ int PmergeMe(char **argv)
 		}
 	}
 
-	vecFordJohnsonSort(vec, 0);
+	fordJohnsonSort(num_vec);
+	// vecFordJohnsonSort(vec, 0);
+
 	// deqFordJohnsonSort(deq);
 
 	std::cout << "Vector:" << std::endl;
-	print_vec(vec);
+	print_numvec(num_vec);
+	// print_vec(vec);
 
+	for (auto value : num_vec)
+		delete value;
 	// std::cout << "Deque:" << std::endl;
 	// for (auto value : deq)
 	// {
