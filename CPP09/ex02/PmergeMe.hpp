9807f1bba37646391ac8338e0b3b13bb997be6c8
Duco Ritsema van Eck <duco.ritsema@am-flow.com>
2025-04-24 14:55:20 +0200

    finalized project, added checking for over maxint value. Added make run command

--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -1,4 +1,7 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include <deque>
 
 typedef struct number_s
 {
@@ -8,12 +11,116 @@ typedef struct number_s
 	number_s *loser;
 } number_t;
 
-void print_number(number_t num);
+template <typename Container = std::vector<number_t *>>
+void print_numcon(Container &con)
+{
+	for (auto value : con)
+	{
+		std::cout << value->value << " ";
+	}
+	std::cout << std::endl;
+}
 
-void print_numvec(std::vector<number_t *> &vec);
+template <typename Container = std::vector<number_t *>>
+void update_indexes_con(Container &con)
+{
+	int i = 0;
+	for (auto value : con)
+	{
+		value->index = i;
+		i++;
+	}
+}
 
-void update_indexes_vec(std::vector<number_t *> &vec);
+template <typename Container = std::vector<number_t *>>
+void insertCon(number_t *num, size_t start_index, size_t end_index, Container &con)
+{
+	size_t sectionSize = end_index - start_index + 1;
+	size_t compare_index = (start_index + end_index) / 2;
 
-void print_numdeq(std::deque<number_t *> &deq);
+	if (sectionSize == 1)
+	{
+		auto it = con.begin() + compare_index;
+		con.insert(it, num);
+		update_indexes_con<Container>(con);
+	}
+	else if (num->value <= con[compare_index]->value)
+	{
+		insertCon<Container>(num, start_index, compare_index, con);
+	}
+	else if (num->value > con[compare_index]->value)
+	{
+		insertCon<Container>(num, compare_index + 1, end_index, con);
+	}
+}
 
-void update_indexes_deq(std::deque<number_t *> &deq);
+template <typename Container = std::vector<number_t *>>
+void fordJohnsonSortCon(Container &vec)
+{
+	Container winners;
+	size_t i = 0;
+
+	for (; i + 1 < vec.size(); i++)
+	{
+		if (vec[i]->value >= vec[i + 1]->value)
+		{
+			auto it = vec.begin() + i;
+			winners.push_back(vec[i]);
+			(*it)->index = winners.size() - 1;
+			(*it)->loser = vec[i + 1];
+			vec[i + 1]->winner = *it;
+			vec.erase(it);
+			update_indexes_con<Container>(vec);
+		}
+		else
+		{
+			auto it = vec.begin() + i + 1;
+			winners.push_back(vec[i + 1]);
+			(*it)->index = winners.size() - 1;
+			(*it)->loser = vec[i];
+			vec[i]->winner = *it;
+			vec.erase(it);
+			update_indexes_con<Container>(vec);
+		}
+	}
+
+	if (winners.size() > 1)
+		fordJohnsonSortCon<Container>(winners);
+	// Start insertion. Use jacobsthal sequence.
+
+	// First we grab the loser connected to the winner on the first index.
+	auto it_winner = winners.begin();
+	auto it_loser = vec.begin() + (*it_winner)->loser->index;
+	winners.insert(it_winner, *it_loser);
+	vec.erase(it_loser);
+	update_indexes_con<Container>(vec);
+	update_indexes_con<Container>(winners);
+
+	size_t cj = 3;
+	size_t pj = 1;
+	i = cj - pj - 1;
+
+	while (vec.size() > 0)
+	{
+		if (i > vec.size() - 1)
+			i = vec.size() - 1;
+		auto it = vec.begin() + i;
+		// If odd one has no winner
+		if ((*it)->winner)
+			insertCon<Container>(*it, 0, (*it)->winner->index, winners);
+		else
+			insertCon<Container>(*it, 0, winners.size() - 1, winners);
+		vec.erase(it);
+		if (i == 0)
+		{
+			size_t tmpj;
+			tmpj = cj + (pj * 2);
+			pj = cj;
+			cj = tmpj;
+			i = cj - pj - 1;
+		}
+		else
+			i--;
+	}
+	vec = winners;
+}
--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -5,188 +5,6 @@
 #include <chrono>
 #include <PmergeMe.hpp>
 
-void insertDeq(number_t *num, size_t start_index, size_t end_index, std::deque<number_t *> &deq)
-{
-	size_t sectionSize = end_index - start_index + 1;
-	size_t compare_index = (start_index + end_index) / 2;
-
-	if (sectionSize == 1)
-	{
-		auto it = deq.begin() + compare_index;
-		deq.insert(it, num);
-		update_indexes_deq(deq);
-	}
-	else if (num->value <= deq[compare_index]->value)
-	{
-		insertDeq(num, start_index, compare_index, deq);
-	}
-	else if (num->value > deq[compare_index]->value)
-	{
-		insertDeq(num, compare_index + 1, end_index, deq);
-	}
-}
-
-void insertVec(number_t *num, size_t start_index, size_t end_index, std::vector<number_t *> &vec)
-{
-	size_t sectionSize = end_index - start_index + 1;
-	size_t compare_index = (start_index + end_index) / 2;
-
-	if (sectionSize == 1)
-	{
-		auto it = vec.begin() + compare_index;
-		vec.insert(it, num);
-		update_indexes_vec(vec);
-	}
-	else if (num->value <= vec[compare_index]->value)
-	{
-		insertVec(num, start_index, compare_index, vec);
-	}
-	else if (num->value > vec[compare_index]->value)
-	{
-		insertVec(num, compare_index + 1, end_index, vec);
-	}
-}
-
-void fordJohnsonSortVec(std::vector<number_t *> &vec)
-{
-	std::vector<number_t *> winners;
-	size_t i = 0;
-
-	for (; i + 1 < vec.size(); i++)
-	{
-		if (vec[i]->value >= vec[i + 1]->value)
-		{
-			auto it = vec.begin() + i;
-			winners.push_back(vec[i]);
-			(*it)->index = winners.size() - 1;
-			(*it)->loser = vec[i + 1];
-			vec[i + 1]->winner = *it;
-			vec.erase(it);
-			update_indexes_vec(vec);
-		}
-		else
-		{
-			auto it = vec.begin() + i + 1;
-			winners.push_back(vec[i + 1]);
-			(*it)->index = winners.size() - 1;
-			(*it)->loser = vec[i];
-			vec[i]->winner = *it;
-			vec.erase(it);
-			update_indexes_vec(vec);
-		}
-	}
-
-	if (winners.size() > 1)
-		fordJohnsonSortVec(winners);
-	// Start insertion. Use jacobsthal sequence.
-
-	// First we grab the loser connected to the winner on the first index.
-	auto it_winner = winners.begin();
-	auto it_loser = vec.begin() + (*it_winner)->loser->index;
-	winners.insert(it_winner, *it_loser);
-	vec.erase(it_loser);
-	update_indexes_vec(vec);
-	update_indexes_vec(winners);
-
-	size_t cj = 3;
-	size_t pj = 1;
-	i = cj - pj - 1;
-
-	while (vec.size() > 0)
-	{
-		if (i > vec.size() - 1)
-			i = vec.size() - 1;
-		auto it = vec.begin() + i;
-		// If odd one has no winner
-		if ((*it)->winner)
-			insertVec(*it, 0, (*it)->winner->index, winners);
-		else
-			insertVec(*it, 0, winners.size() - 1, winners);
-		vec.erase(it);
-		if (i == 0)
-		{
-			size_t tmpj;
-			tmpj = cj + (pj * 2);
-			pj = cj;
-			cj = tmpj;
-			i = cj - pj - 1;
-		}
-		else
-			i--;
-	}
-	vec = winners;
-}
-
-void fordJohnsonSortDeq(std::deque<number_t *> &deq)
-{
-	std::deque<number_t *> winners;
-	size_t i = 0;
-
-	for (; i + 1 < deq.size(); i++)
-	{
-		if (deq[i]->value >= deq[i + 1]->value)
-		{
-			auto it = deq.begin() + i;
-			winners.push_back(deq[i]);
-			(*it)->index = winners.size() - 1;
-			(*it)->loser = deq[i + 1];
-			deq[i + 1]->winner = *it;
-			deq.erase(it);
-			update_indexes_deq(deq);
-		}
-		else
-		{
-			auto it = deq.begin() + i + 1;
-			winners.push_back(deq[i + 1]);
-			(*it)->index = winners.size() - 1;
-			(*it)->loser = deq[i];
-			deq[i]->winner = *it;
-			deq.erase(it);
-			update_indexes_deq(deq);
-		}
-	}
-
-	if (winners.size() > 1)
-		fordJohnsonSortDeq(winners);
-	// Start insertion. Use jacobsthal sequence.
-
-	// First we grab the loser connected to the winner on the first index.
-	auto it_winner = winners.begin();
-	auto it_loser = deq.begin() + (*it_winner)->loser->index;
-	winners.insert(it_winner, *it_loser);
-	deq.erase(it_loser);
-	update_indexes_deq(deq);
-	update_indexes_deq(winners);
-
-	size_t cj = 3;
-	size_t pj = 1;
-	i = cj - pj - 1;
-
-	while (deq.size() > 0)
-	{
-		if (i > deq.size() - 1)
-			i = deq.size() - 1;
-		auto it = deq.begin() + i;
-		// If odd one has no winner
-		if ((*it)->winner)
-			insertDeq(*it, 0, (*it)->winner->index, winners);
-		else
-			insertDeq(*it, 0, winners.size() - 1, winners);
-		deq.erase(it);
-		if (i == 0)
-		{
-			size_t tmpj;
-			tmpj = cj + (pj * 2);
-			pj = cj;
-			cj = tmpj;
-			i = cj - pj - 1;
-		}
-		else
-			i--;
-	}
-	deq = winners;
-}
-
 int PmergeMe(char **argv)
 {
 	std::deque<number_t *> num_deq;
@@ -201,20 +19,28 @@ int PmergeMe(char **argv)
 		argument = argv[i];
 		if (std::regex_match(argument, match, pattern))
 		{
-			int num = std::stoi(argument);
+			try
+			{
+				int num = std::stoi(argument);
 
-			number_t *numt = new number_t;
-			number_t *numpty = new number_t;
-			numt->index = num_vec.size() - 1;
-			numt->value = num;
-			numt->loser = 0;
-			numt->winner = 0;
-			numpty->index = num_deq.size() - 1;
-			numpty->value = num;
-			numpty->loser = 0;
-			numpty->winner = 0;
-			num_vec.push_back(numt);
-			num_deq.push_back(numpty);
+				number_t *numt = new number_t;
+				number_t *numpty = new number_t;
+				numt->index = num_vec.size() - 1;
+				numt->value = num;
+				numt->loser = 0;
+				numt->winner = 0;
+				numpty->index = num_deq.size() - 1;
+				numpty->value = num;
+				numpty->loser = 0;
+				numpty->winner = 0;
+				num_vec.push_back(numt);
+				num_deq.push_back(numpty);
+			}
+			catch (std::exception &e)
+			{
+				std::cerr << "Error: number is invalid. (Over maxint etc)" << std::endl;
+				return 1;
+			}
 		}
 		else
 		{
@@ -224,18 +50,18 @@ int PmergeMe(char **argv)
 	}
 
 	std::cout << "Before sort:" << std::endl;
-	print_numvec(num_vec);
+	print_numcon<std::vector<number_t *>>(num_vec);
 
 	std::chrono::high_resolution_clock::time_point begin = std::chrono::high_resolution_clock::now();
-	fordJohnsonSortVec(num_vec);
+	fordJohnsonSortCon(num_vec);
 	std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
 
 	std::chrono::high_resolution_clock::time_point begin2 = std::chrono::high_resolution_clock::now();
-	fordJohnsonSortDeq(num_deq);
+	fordJohnsonSortCon(num_deq);
 	std::chrono::high_resolution_clock::time_point end2 = std::chrono::high_resolution_clock::now();
 
 	std::cout << "After sort:" << std::endl;
-	print_numvec(num_vec);
+	print_numcon<std::vector<number_t *>>(num_vec);
 
 	// Print timing
 	std::chrono::microseconds elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
